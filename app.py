3ee8d3c0895b339f40c8aafe383379d615f81091
Abhishek Pandey <Abhishekpandey02>
2026-04-17 21:00:38 +0530

    yml added

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
-# MongoDB connection
-MONGO_URL = "mongodb://admin:qwerty@localhost:27017"
-client = MongoClient(MONGO_URL)
+# Load environment variables
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "abhi")
 
-db = client["abhi"]
+# MongoDB connection
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
 collection = db["users"]
 
-# Home page (form)
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Show users in browser
 @app.route("/getUsers")
 def get_users_page():
     users = list(collection.find({}, {"_id": 0}))
     return render_template("users.html", users=users)
 
-# API: Get users (JSON)
 @app.route("/api/getUsers", methods=["GET"])
 def get_users():
     users = list(collection.find({}, {"_id": 0}))
     return jsonify(users)
 
-# API: Add user
 @app.route("/addUser", methods=["POST"])
 def add_user():
     data = request.form
@@ -40,8 +39,7 @@ def add_user():
     }
 
     collection.insert_one(user)
-
     return render_template("index.html", message="User added successfully!")
 
 if __name__ == "__main__":
-    app.run(port=5050, debug=True)
+    app.run(host="0.0.0.0", port=5050)
